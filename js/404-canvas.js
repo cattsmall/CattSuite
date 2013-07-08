99fada286533a4320f12302d629dac450ce5a74b
Catt Small <cattsmall@gmail.com>
2013-07-08 00:11:19 -0400

    Images ready for animating….eventually.

--- js/404-canvas.js
+++ js/404-canvas.js
@@ -1,21 +1,35 @@
 $(document).ready(function() {
 	var canvas = $("#game-404-canvas");
-	canvas.attr("width", canvas.width());
-	canvas.attr("height", canvas.height());
-	
-	var canvasWidth = canvas.width();
-	var canvasHeight = canvas.height();
+	function resizeCanvas() {
+		canvas.attr("width", canvas.width());
+		canvas.attr("height", canvas.height());
+		
+		var canvasWidth = canvas.width();
+		var canvasHeight = canvas.height();
 
-	var context = canvas.get(0).getContext("2d");
-	context.beginPath(); // Start the path
-	context.moveTo(25, canvasHeight - 100); // Set the path origin
-	context.lineTo(canvasWidth - 25, canvasHeight - 100); // Set the path destination
-	context.closePath(); // Close the path
-	context.stroke(); // Outline the path
+		var context = canvas.get(0).getContext("2d");
+		context.beginPath(); // Start the path
+		context.moveTo(25, canvasHeight - 100); // Set the path origin
+		context.lineTo(canvasWidth - 25, canvasHeight - 100); // Set the path destination
+		context.closePath(); // Close the path
+		context.stroke(); // Outline the path
 	
-	context.strokeRect(25, 25, canvasWidth - 50, canvasHeight - 50);
+		context.beginPath(); // rectangle
+		context.lineWidth = 3;
+		context.strokeRect(25, 25, canvasWidth - 50, canvasHeight - 50);
 	
-	var text = "Hello, World!";
-	context.fillText(text, 40, 40);
-	context.font = "italic 30px serif";
+		context.font = "24px Quicksand";
+		context.fillText("Hello, World!", 40, 100);
+		context.fillText("Press here to play.", 40, 150);
+		
+		var catt = new Image();
+		catt.src = "wp-content/themes/CATT6-2013_theme/img/canvas/catt/catt-standing.png";
+		catt.onload = function(){
+			context.drawImage(catt, canvasWidth/2 - 125, 40);
+		};
+	}
+	
+	resizeCanvas();
+	
+	$(window).resize(resizeCanvas);
 });
