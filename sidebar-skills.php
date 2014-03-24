d2298fbb5946884986c7c15a9a39f1e7ca80c4aa
Catt Small <cattsmall@gmail.com>
2014-03-24 08:45:32 -0400

    Updated skills & analytics
    I wasn't properly evaluating my skills and was only adding to the percentages I put before. Yikes. Preparing to also possibly take out print & branding from the skills section since it's less relevant. The whole thing just needs rethinking.

--- sidebar-skills.php
+++ sidebar-skills.php
@@ -9,52 +9,52 @@
 			<article class="hidden-tablet-vert">
 				<header><h3>Digital</h3></header>
 				<ul>
-					<li rel="tooltip" title="I've designed websites of all kinds including web apps and ecommerce sites. I enjoy designing and improving user experiences." data-placement="top">
+					<li rel="tooltip" title="I've designed websites of all kinds, including web apps, public-facing websites, and ecommerce sites. I love to help people get things done." data-placement="top">
 						<h4>Web &amp; UX Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 100%;"></div>
+						  <div class="bar" style="width: 90%;"></div>
 						</div>
 					</li>
-					<li rel="tooltip" title="Media queries and percentages are my best friends! I love adapting designs for all screen sizes and think mobile first." data-placement="top">
+					<li rel="tooltip" title="When I design websites, I make them look good for all screen sizes. I can also adapt older designs for all screen sizes." data-placement="top">
 						<h4>Responsive Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 90%;"></div>
+						  <div class="bar" style="width: 80%;"></div>
 						</div>
 					</li>
 					<li rel="tooltip" title="I can't code for iOS or Android, but I design beautiful interfaces and code in HTML5." data-placement="top">
 						<h4>App Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 80%;"></div>
+						  <div class="bar" style="width: 70%;"></div>
 						</div>
 					</li>
 					<li rel="tooltip" title="I love coding and staying up-to-date with the latest versions of HTML &amp; CSS3." data-placement="top">
 						<h4>Valid HTML5 &amp; CSS3</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 95%;"></div>
+						  <div class="bar" style="width: 85%;"></div>
 						</div>
 					</li>
 					<li rel="tooltip" title="I have heavy amounts of experience with JavaScript and various frameworks, including jQuery and AngularJS." data-placement="top">
 						<h4>JavaScript</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 90%;"></div>
+						  <div class="bar" style="width: 75%;"></div>
 						</div>
 					</li>
-					<li rel="tooltip" title="I use PHP with Wordpress quite often, and I've built my own MySQL database!" data-placement="top">
+					<li rel="tooltip" title="I have used PHP to create many public-facing websites, as well as to prototype new designs very quickly." data-placement="top">
 						<h4>PHP &amp; MySQL</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 70%;"></div>
+						  <div class="bar" style="width: 50%;"></div>
 						</div>
 					</li>
-					<li rel="tooltip" title="I'm still in the process of learning..." data-placement="top">
+					<li rel="tooltip" title="I'm still in the process of learning Ruby on Rails but have some experience with it." data-placement="top">
 						<h4>Ruby on Rails</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 20%;"></div>
+						  <div class="bar" style="width: 10%;"></div>
 						</div>
 					</li>
-					<li rel="tooltip" title="I have a lot of experience with designing email newsletters, but have a little to learn." data-placement="top">
+					<li rel="tooltip" title="I have a lot of experience with designing email newsletters. I create scalable designs that look good on small &amp; large screens." data-placement="top">
 						<h4>Email Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 95%;"></div>
+						  <div class="bar" style="width: 85%;"></div>
 						</div>
 					</li>
 				</ul>
@@ -62,52 +62,52 @@
 			<article class="hidden-tablet-vert">
 				<header><h3>Print &amp; Branding</h3></header>
 				<ul>
-					<li rel="tooltip" title="I've made &amp; printed high-quality posters. I've even done screenprinting!" data-placement="top">
+					<li rel="tooltip" title="I've designed, printed, &amp; cut many high-quality posters. I've also done screenprinting!" data-placement="top">
 						<h4>Poster Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 100%;"></div>
+						  <div class="bar" style="width: 50%;"></div>
 						</div>
 					</li>
-					<li rel="tooltip" title="I can design and bind books; I've even designed some covers for Scholastic. I love making notebooks!" data-placement="top">
+					<li rel="tooltip" title="I can design and bind books; I've even designed some covers for Scholastic. In my spare time, I enjoy making notebooks." data-placement="top">
 						<h4>Book Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 100%;"></div>
+						  <div class="bar" style="width: 50%;"></div>
 						</div>
 					</li>
-					<li rel="tooltip" title="I've designed two separate magazines and promotional flyers." data-placement="top">
+					<li rel="tooltip" title="I've designed two separate magazines and many promotional flyers." data-placement="top">
 						<h4>Editorial Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 100%;"></div>
+						  <div class="bar" style="width: 45%;"></div>
 						</div>
 					</li>
-					<li rel="tooltip" title="I LOVE packaging, but I don't have access to cutting machines yet!" data-placement="top">
+					<li rel="tooltip" title="I enjoy designing packaging for products, but don't have the opportunity to do so very often." data-placement="top">
 						<h4>Packaging Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 60%;"></div>
+						  <div class="bar" style="width: 30%;"></div>
 						</div>
 					</li>
 					<li rel="tooltip" title="I've created several logos for different brands. Building an identity for a company is amazing." data-placement="top">
 						<h4>Logo Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 90%;"></div>
+						  <div class="bar" style="width: 60%;"></div>
 						</div>
 					</li>
 					<li rel="tooltip" title="I love figuring out new ways to design &amp; fold-up information." data-placement="top">
 						<h4>Newsletter Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 100%;"></div>
+						  <div class="bar" style="width: 60%;"></div>
 						</div>
 					</li>
-					<li rel="tooltip" title="Thinking of new ways to convey information on tiny rectangles makes me happy." data-placement="top">
+					<li rel="tooltip" title="Finding new ways to convey important information in such a small amount of space is a challenge I enjoy." data-placement="top">
 						<h4>Business Card Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 90%;"></div>
+						  <div class="bar" style="width: 70%;"></div>
 						</div>
 					</li>
-					<li rel="tooltip" title="I enjoy designing letterhead since it's not something I get to do often." data-placement="top">
+					<li rel="tooltip" title="I don't get to design stationery very often, but I do have an intense fondness for it." data-placement="top">
 						<h4>Letterhead Design</h4>
 						<div class="progress">
-						  <div class="bar" style="width: 80%;"></div>
+						  <div class="bar" style="width: 60%;"></div>
 						</div>
 					</li>
 				</ul>
