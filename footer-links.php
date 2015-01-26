fc5907befbd3c1ca434592b4d307765a8fb0f002
Catt Small <cattsmall@gmail.com>
2015-01-26 17:36:04 -0500

    Site footer includes RSS + main footer code templatized

---
+++ footer-links.php
@@ -0,0 +1,8 @@
+<footer class="page-footer">
+		<nav>
+         <p>Website design &amp; portfolio work © Catt Small <script type="text/javascript"> var date = new Date();
+		  var year = date.getFullYear();
+		  document.write(year);</script>. Stalk me on: <a href="http://github.com/cattsmall">GitHub</a>, <a href="http://twitter.com/cattsmall">Twitter</a>, <a href="http://www.linkedin.com/in/cattsmall/">LinkedIn</a>, <a href="http://letter-q.tumblr.com">Tumblr</a>, <a href="http://cattsmall.com/feed/">RSS</a>.</p>
+     </nav>
+	<div class="clearfix"></div>
+</footer>
--- footer-portfolio-inner.php
+++ footer-portfolio-inner.php
@@ -1,12 +1,5 @@
    <!-- footer -->
-   <footer class="page-footer">
-   		<nav>
-            <p>Website design &amp; portfolio work © Catt Small <script type="text/javascript"> var date = new Date();
-   		  var year = date.getFullYear();
-   		  document.write(year);</script>. Stalk me on: <a href="http://github.com/cattsmall">GitHub</a>, <a href="http://twitter.com/cattsmall">Twitter</a>, <a href="http://www.linkedin.com/in/cattsmall/">LinkedIn</a>, <a href="http://letter-q.tumblr.com">Tumblr</a>.</p>
-        </nav>
-   	<div class="clearfix"></div>
-   </footer>
+   <?php include_once("footer-links.php") ?>
 
    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.min-ck.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min-ck.js"></script>
